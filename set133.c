b1903779a87b3639658a72c21216497c4c8703ce
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-03-17 19:26:58 +0530

    Create set133.c

---
+++ set133.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,n,m,c=0;
+    scanf("%d %d",&n,&m);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d %d",&a,&b);
+        if(b==m)
+        {
+            c++;
+        }
+    }
+    if(c>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
